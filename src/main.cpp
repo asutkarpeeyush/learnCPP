09ef0851678abe7f88219ba35e343a28ab1ce9cd
riteshasutkar <asutkarritesh@gmail.com>
2022-02-26 23:36:56 +0530

    strings tutorial

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cstring>
+#include <string>
 #include <array>
 #include "fun.h"
 #include "Log.h"
@@ -93,6 +93,8 @@ struct StaticStructExample {
     }
 };
 
+void PrintCPPString(std::string basicString);
+
 int StaticStructExample::x;
 int StaticStructExample::y;
 
@@ -102,6 +104,11 @@ enum EnumExample : char {
 //    A, B, C
     A = 0, B = 2, C = 5
 };
+
+void AlterCPPString(std::string& cppString) {
+    cppString += "hhh";
+}
+
 
 int main() {
     int a;
@@ -227,31 +234,56 @@ int main() {
     // public - from everywhere
 
     // ------------------- Arrays ------------------------ //
-    int example[5];
-    int* examplePtr = example;
-    example[0] = 3;
-    *(examplePtr + 2) = 7;
-    *(int*)((char*)examplePtr + 8) = 10; //  this is similar to above line
-    example[4] = 6;
+//    int example[5];
+//    int* examplePtr = example;
+//    example[0] = 3;
+//    *(examplePtr + 2) = 7;
+//    *(int*)((char*)examplePtr + 8) = 10; //  this is similar to above line
+//    example[4] = 6;
+//
+//    int* anotherArr = new int[5]; // this is created on heap and need to be destroyed
+//    // or need will be deleted when program ends (not the scope but program)
+//    // the use case is when a function has to return an array, it has to live outside the scope
+//    // by creating on heap has a downside which is jumping around memory and that is a performance hit
+//
+//    // there is no method to get the size of array if it's created on heap
+//    anotherArr[3] = 6;
+//
+//    LOG(sizeof(example)/sizeof(int));
+//    delete[] anotherArr;
+//
+//    // correct way to keep size
+//    static const int arrSize = 5;
+//    int arr[arrSize];
+//
+//    // cpp11
+//    std::array<int, 50> cppArray;
+//    LOG(cppArray.size());
+//
+//    // 2d array
+//    int twodArr[3][2] = {{1,2}, {3,4}, {5,6}};
+//    LOG(twodArr[1][1]);
+//
+//    LOG(example);
 
-    int* anotherArr = new int[5]; // this is created on heap and need to be destroyed
-    // or need will be deleted when program ends (not the scope but program)
-    // the use case is when a function has to return an array, it has to live outside the scope
-    // by creating on heap has a downside which is jumping around memory and that is a performance hit
+    // ------------------- Strings ------------------------ //
+    char* name = "Piyush"; //\o is null termination
+    // *name = 'g'; // not allowed in some compilers
+    // https://stackoverflow.com/questions/164194/why-do-i-get-a-segmentation-fault-when-writing-to-a-char-s-initialized-with-a
+    LOG(name);
 
-    // there is no method to get the size of array if it's created on heap
-    anotherArr[3] = 6;
+    char name2[5] = {'P', 'i', 'y', 'u', '\0'};
+    LOG(name2);
 
-    LOG(sizeof(example)/sizeof(int));
-    delete[] anotherArr;
+    std::string cppName = "Cherno";
+    cppName += " Hello";
+    // std::string cppName = std::string("Cherno") +  "Hello";
+    bool contains = cppName.find("no") != std::string::npos;
+    LOG(contains);
+    LOG(cppName.size());
+    LOG(cppName);
 
-    // correct way to keep size
-    static const int arrSize = 5;
-    int arr[arrSize];
-
-    // cpp11
-    std::array<int, 50> cppArray;
-    LOG(cppArray.size());
-
-    LOG(example);
+    // always pass strings by const reference or reference
+    AlterCPPString(cppName);
+    LOG(cppName);
 }
